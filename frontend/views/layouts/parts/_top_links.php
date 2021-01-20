e85b5ac7adc9201ff559051f38b53a3b289c3c0d
topalek <v.topalek@gmail.com>
2021-01-20 14:12:05 +0200

    topLinks rebuild into dropdown nav

--- frontend/views/layouts/parts/_top_links.php
+++ frontend/views/layouts/parts/_top_links.php
@@ -1,16 +1,39 @@
 <?php
 
+use yii\bootstrap4\Nav;
 use yii\helpers\Html;
 
 ?>
 
-<?= Html::a('About', ['/site/signup']) ?>
+<?= Html::a('About', ['/site/about']) ?>
 <?php
 if (Yii::$app->user->isGuest): ?>
     <?= Html::a('Sign in', ['/site/sign-in']) ?>
     <?= Html::a('Signup', ['/site/signup']) ?>
 <?php
 else: ?>
-    <?= Html::a('Logout (' . Yii::$app->user->identity->username . ')', ['/site/logout'], ['data-method' => 'post']) ?>
+    <?= Nav::widget(
+        [
+            'items' => [
+                [
+                    'label'           => Yii::$app->user->identity->getDisplayName(),
+                    'items'           => [
+                        [
+                            'label' => 'Profile',
+                            'url'   => ['/user/profile', 'id' => Yii::$app->user->identity->getId()],
+                        ],
+                        [
+                            'label'       => 'Logout',
+                            'url'         => ['/site/logout'],
+                            'linkOptions' => [
+                                'data-method' => 'post',
+                            ],
+                        ],
+                    ],
+                    'dropdownOptions' => ['class' => 'dropdown-menu-right'],
+                ],
+            ],
+        ]
+    ); ?>
 <?php
 endif; ?>
